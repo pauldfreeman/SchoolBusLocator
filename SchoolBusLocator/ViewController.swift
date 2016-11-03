0d597532174043ee0c4544793f65f2316c4784b1
pauldfreeman <paul@pauls-mac-mini.local>
2016-11-03 20:07:38 +0000

    updated to receive delimited position

--- SchoolBusLocator/ViewController.swift
+++ SchoolBusLocator/ViewController.swift
@@ -12,17 +12,31 @@ import CoreLocation
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var lblCurrentLat: UILabel!
+    @IBOutlet weak var lblCurrentLon: UILabel!
+    @IBOutlet weak var lblHorizAcc: UILabel!
+    @IBOutlet weak var lblSpeedMPH: UILabel!
+
+    @IBOutlet weak var lblCurrentTime: UILabel!
+    @IBOutlet weak var lblPositionTime: UILabel!
+    @IBOutlet weak var lblDelay: UILabel!
+    
     @IBOutlet weak var btnSubscribe: UIButton!
+    @IBOutlet weak var btnUnsubscribe: UIButton!
+    
     
     var locationManager: CLLocationManager = CLLocationManager()
     var startLocation: CLLocation!
+    var channel: ARTRealtimeChannel!
     
     var client: ARTRealtime = ARTRealtime(key: "QGOsVA.UnM4VQ:YuOO9DIWTgs2BcPZ")
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        //let client =
+        
+        channel = client.channels.get("Position")
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,11 +45,14 @@ class ViewController: UIViewController {
     }
 
     @IBAction func clicked(_ sender: AnyObject) {
-        let channel = client.channels.get("Position")
         
         channel.subscribe{ message in
-            debugPrint(message.data)
+            debugPrint(message.data as! String)
         }
     }
 
+    @IBAction func unsubscribe_clicked(_ sender: Any) {
+    
+        channel.unsubscribe()
+    }
 }
